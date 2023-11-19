f3c0d519aed250c29a19a1e0fbdfd19dfefd5732
Guo Siqi <guosiqi@Guos-MBP.lan>
2023-11-18 23:29:44 -0500

    draft export failed

--- Canvas-GAN/Canvas-GAN/DoodleView.swift
+++ Canvas-GAN/Canvas-GAN/DoodleView.swift
@@ -13,6 +13,8 @@ struct DoodleView: View {
     @Environment(ViewModel.self) private var viewModel
     @Environment(\.dismissWindow) private var dismissWindow
     
+    var convertView: some View { return VStack { DrawingView() } }
+    
     var body: some View {
         VStack {
             DrawingView()
@@ -21,6 +23,20 @@ struct DoodleView: View {
                 .padding()
             
             Button("Done") {
+                // @https://developer.apple.com/videos/play/wwdc2023/111215/?time=176
+                // @https://clipdrop.co/apis/docs/sketch-to-image
+                // Pity that trapped here,
+                // Back if possible
+                let image = UIImage(named: "think_different")
+                if image == nil { print("No") }
+                
+                let renderer = ImageRenderer(content: body)
+                if let img = renderer.uiImage {
+                    let filename_2 = NSHomeDirectory() + "/doodle.png"
+                    print(type(of: img))
+                    try? img.pngData()?.write(to: URL(fileURLWithPath: filename_2))
+                }
+                
                 dismissWindow(id: "doodle_canvas")
                 viewModel.flowState = .PROJECTILEFLYING
             }
@@ -40,10 +56,15 @@ struct DrawingView: UIViewRepresentable {
 }
 
 
+//extension UIView {
+//}
+
 /* UIView in UIKit */
 class DrawingUIView: UIView {
     private var path = UIBezierPath()
     private var strokeWidth: CGFloat = 5.0
+    private var width_drawing = 0.0
+    private var height_drawing = 0.0
     
     // ======== Init ========
     override init(frame: CGRect) {
@@ -60,6 +81,8 @@ class DrawingUIView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
+        width_drawing = CGRectGetWidth(rect)
+        height_drawing = CGRectGetHeight(rect)
         UIColor.black.setStroke()
         path.stroke()
     }
@@ -74,4 +97,14 @@ class DrawingUIView: UIView {
         path.addLine(to: touch.location(in: self))
         setNeedsDisplay()
     }
+//    func asImage(backgroundColor: UIColor = .white, strokeColor: UIColor = .black) -> UIImage? {
+//        let size = CGSize(width: width_drawing, height: height_drawing)
+//        let renderer = UIGraphicsImageRenderer(size: size)
+//        let image = renderer.image { ctx in
+//            backgroundColor.setFill()
+//            strokeColor.setStroke()
+//            path.stroke()
+//        }
+//        return image
+//    }
 }
